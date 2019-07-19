3f110895a36be201ac32a5f761a60b54d1c00d6d
Yury Kudryavtsev <kudryavtsev@greensight.ru>
2019-07-19 17:28:25 +0300

    #43812 migrations

---
+++ database/migrations/2019_07_19_093143_create_discounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyInteger('type');
+            $table->string('name');
+            $table->tinyInteger('value_type');
+            $table->integer('value');
+            $table->integer('region_id');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+}
