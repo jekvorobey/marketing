a47c77236083cad5a377b85834fbcfd363caf048
Yury Kudryavtsev <kudryavtsev@greensight.ru>
2019-08-05 17:21:36 +0300

    #47377 discounts seeder fix

--- database/seeds/DiscountsTableSeeder.php
+++ database/seeds/DiscountsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Models\Discount;
+use App\Models\Discount\Discount;
 
 /**
  * Class DiscountsTableSeeder
@@ -23,14 +23,21 @@ class DiscountsTableSeeder extends Seeder
         for ($i=0; $i<= 50; $i++) {
             $discount = new Discount();
             $discount->merchant_id = $merchants[array_rand($merchants)];
-            $discount->type = rand(1, 2);
+           // 1 - скидка, 2 - промокод
+            if(rand(0, 1)) {
+                $discount->type = 1;
+            } else {
+                $discount->type = 2;
+                $discount->promo_code = $faker->regexify('[A-Z0-9]{7}');
+            }
+
             $discount->name = $faker->text(100);
-            // Тип значения (0 - проценты, 1 - рубли)
+            // Тип значения (1 - проценты, 2 - рубли)
             if(rand(0, 1)) {
                 $discount->value_type = 1;
                 $discount->value = rand(10, 1000);
             } else {
-                $discount->value_type = 0;
+                $discount->value_type = 2;
                 $discount->value = rand(5, 15);
             }
             $discount->region_id = null;
