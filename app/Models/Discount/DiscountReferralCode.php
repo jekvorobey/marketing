c7384aab057a760570e3f3113152f5ac769b2052
Yury Kudryavtsev <kudryavtsev@greensight.ru>
2019-08-22 18:21:36 +0300

    #48099 Discount referal model fix

--- app/Models/Discount/DiscountReferralCode.php
+++ app/Models/Discount/DiscountReferralCode.php
@@ -5,13 +5,11 @@ namespace App\Models\Discount;
 use Greensight\CommonMsa\Models\AbstractModel;
 
 /**
- * Класс-модель для сущности "Скидка для
- *
- * "
+ * Класс-модель для сущности "Скидка для реферала"
  * App\Models\Discount\Discount
  *
  * @property int $discount_id
- * @property float $min_summ
+ * @property string $referral_code
  * @mixin \Eloquent
  *
  */
@@ -20,7 +18,7 @@ class DiscountReferralCode extends AbstractModel
     /**
      * Заполняемые поля модели
      */
-    const FILLABLE = ['discount_id', 'min_summ'];
+    const FILLABLE = ['discount_id', 'referral_code'];
 
     /**
      * @var array
@@ -30,5 +28,5 @@ class DiscountReferralCode extends AbstractModel
     /**
      * @var string
      */
-    protected $table = 'discount_cart_summ';
+    protected $table = 'discount_referral_codes';
 }
