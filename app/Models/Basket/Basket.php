d8fba597ddddc16373ccf1e1c687f9053008c29b
Ivan Koryukov <madridianfox@yandex.ru>
2019-11-19 15:59:09 +0300

    #52535 поправил расчёт цен и структуру ответа

--- app/Models/Basket/Basket.php
+++ app/Models/Basket/Basket.php
@@ -8,6 +8,22 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class Basket implements \JsonSerializable
 {
+    /**
+     * Стоимость корзины без скидок.
+     * @var float
+     */
+    public $cost;
+    /**
+     * Итоговая цена всей корзины (cost - discount).
+     * @var float
+     */
+    public $price;
+    /**
+     * Размер скидки.
+     * @var float
+     */
+    public $discount;
+    
     /** @var int */
     public $user;
     /** @var string */
@@ -16,11 +32,6 @@ class Basket implements \JsonSerializable
     public $deliveryMethod;
     /** @var int */
     public $payMethod;
-    
-    /** @var float */
-    public $price;
-    /** @var float */
-    public $discount;
     /** @var BasketItem[] */
     public $items;
     
@@ -30,6 +41,7 @@ class Basket implements \JsonSerializable
             'user' => 'required|integer',
             'items' => 'required|array',
             'items.*.id' => 'required|integer',
+            'items.*.qty' => 'required|integer',
             'items.*.offer_id' => 'required|integer',
             'items.*.brand_id' => 'required|integer',
             'items.*.category_id' => 'required|integer',
@@ -47,8 +59,14 @@ class Basket implements \JsonSerializable
         $basket->payMethod = $data['pay_method'] ?? null;
         
         foreach ($data['items'] as $itemData) {
-            ['id' => $id, 'offer_id' => $offerId, 'category_id' => $categoryId, 'brand_id' => $brandId] = $itemData;
-            $basket->items[] = new BasketItem($id, $offerId, $categoryId, $brandId);
+            [
+                'id' => $id,
+                'qty' => $qty,
+                'offer_id' => $offerId,
+                'category_id' => $categoryId,
+                'brand_id' => $brandId
+            ] = $itemData;
+            $basket->items[] = new BasketItem($id, $qty, $offerId, $categoryId, $brandId);
         }
         
         return $basket;
@@ -65,24 +83,36 @@ class Basket implements \JsonSerializable
             return $item->offerId;
         }, $this->items);
         $prices = Price::query()->whereIn('offer_id', $ids)->get()->keyBy('offer_id');
-        $totalSum = 0;
+        $totalCost = 0;
+        $totalItemDiscount = 0;
         // todo добавить расчёт скидок
         foreach ($this->items as $item) {
             if ($prices->has($item->offerId)) {
-                $item->price = $prices[$item->offerId]->price;
-                $totalSum += $item->price;
+                $item->cost = $prices[$item->offerId]->price;
+                $item->totalCost = $item->cost * $item->qty;
+                $item->discount = 0 * $item->qty;
+                $item->price = $item->totalCost - $item->discount;
+                
+                $totalCost += $item->totalCost;
+                $totalItemDiscount += $item->discount;
             } else {
                 throw new \Exception("basket item offer {$item->offerId} without price");
             }
         }
-        $this->price = $totalSum;
+        
+        $basketDiscount = 0;
+        $this->cost = $totalCost;
+        $this->discount = $totalItemDiscount + $basketDiscount;
+        $this->price = $totalCost - $this->discount;
     }
     
     public function jsonSerialize()
     {
         return [
+            'cost' => $this->cost,
             'price' => $this->price,
             'discount' => $this->discount,
+            
             'items' => $this->items,
         ];
     }
--- app/Models/Basket/BasketItem.php
+++ app/Models/Basket/BasketItem.php
@@ -6,11 +6,37 @@ class BasketItem implements \JsonSerializable
 {
     /** @var int */
     public $id;
-    /** @var float */
+    
+    /**
+     * Стоимость единцы товара без скидок.
+     * @var float
+     */
+    public $cost;
+    /**
+     * Стоимость элемента корзины без скидок (cost*qty).
+     * @var float
+     */
+    public $totalCost;
+    /**
+     * Цена элемента корзины со скидкой (totalCost - discount).
+     * @var float
+     */
     public $price;
-    /** @var float */
+    /**
+     * Рамер скидки.
+     * @var float
+     */
     public $discount;
-    /** @var int */
+
+    /**
+     * Количество единиц товара в элементе корзины.
+     * @var int
+     */
+    public $qty;
+    /**
+     * Id товара (предложения мерчанта).
+     * @var int
+     */
     public $offerId;
     /** @var int */
     public $categoryId;
@@ -20,24 +46,29 @@ class BasketItem implements \JsonSerializable
     /**
      * BasketItem constructor.
      * @param int $id
+     * @param int $qty
      * @param int $offerId
      * @param int $categoryId
      * @param int $brandId
      */
-    public function __construct(int $id, int $offerId, int $categoryId, int $brandId)
+    public function __construct(int $id, int $qty, int $offerId, int $categoryId, int $brandId)
     {
         $this->id = $id;
         $this->offerId = $offerId;
         $this->categoryId = $categoryId;
         $this->brandId = $brandId;
+        $this->qty = $qty;
     }
     
     public function jsonSerialize()
     {
         return [
             'id' => $this->id,
+            'qty' => $this->qty,
             'price' => $this->price,
-            'discount' => $this->discount
+            'discount' => $this->discount,
+            'totalCost' => $this->totalCost,
+            'cost' => $this->cost,
         ];
     }
 }
