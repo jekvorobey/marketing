c703a9e7bc11e027c89c9ac6e5d445ba899b6903
Anton Filippov <filippov@greensight.ru>
2020-10-07 10:18:34 +0300

    fix Откат задачи 66360.

--- app/Services/Calculator/AbstractCalculator.php
+++ app/Services/Calculator/AbstractCalculator.php
@@ -90,7 +90,7 @@ abstract class AbstractCalculator
      * @param int  $lowestPossiblePrice Самая низкая возможная цена (по умолчанию = 1 рубль)
      * @param Discount  $discount
      *
-     * @return float
+     * @return int
      */
     protected function changePrice(
         &$item,
@@ -130,7 +130,8 @@ abstract class AbstractCalculator
 
             if ($apply) {
                 $offerInBundle['discount'] = $currentDiscount + $discountValue;
-                $offerInBundle['price']    = self::round($currentCost - $offerInBundle['discount'], self::ROUND);
+                $offerInBundle['price']    = $currentCost - $offerInBundle['discount'];
+//                $offerInBundle['price']    = self::round($currentCost - $offerInBundle['discount'], self::ROUND);
             }
         } else {
             $currentDiscount = $item['discount'] ?? 0;
@@ -144,7 +145,8 @@ abstract class AbstractCalculator
 
             if ($apply) {
                 $item['discount'] = $currentDiscount + $discountValue;
-                $item['price']    = self::round($currentCost - $item['discount'], self::ROUND);
+                $item['price']    = $currentCost - $item['discount'];
+//                $item['price']    = self::round($currentCost - $item['discount'], self::ROUND);
                 $item['cost']     = $currentCost;
             }
         }
@@ -157,13 +159,14 @@ abstract class AbstractCalculator
      * @param $value
      * @param $valueType
      *
-     * @return float
+     * @return int
      */
     protected function calculateDiscountByType($cost, $value, $valueType)
     {
         switch ($valueType) {
             case Discount::DISCOUNT_VALUE_TYPE_PERCENT:
-                return $cost * $value / 100;
+                return round($cost * $value / 100);
+//                return $cost * $value / 100;
             case Discount::DISCOUNT_VALUE_TYPE_RUB:
                 return $value;
             default:
