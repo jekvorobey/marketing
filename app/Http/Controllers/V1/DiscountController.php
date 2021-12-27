cab9f95379b5f7da3332e6673d088b3d289584b5
Max Baranov <bm1812@yandex.ru>
2021-12-27 11:14:44 +0300

    IBT-553: comment field for creating

--- app/Http/Controllers/V1/DiscountController.php
+++ app/Http/Controllers/V1/DiscountController.php
@@ -229,6 +229,7 @@ class DiscountController extends Controller
                 'product_qty_limit' => 'numeric|nullable',
                 'merchant_id' => 'numeric|nullable',
                 'relations' => 'array',
+                'comment' => 'string|nullable',
             ]);
 
             $data['user_id'] = $client->userId();
