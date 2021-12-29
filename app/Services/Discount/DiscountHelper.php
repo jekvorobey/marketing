e45ab9f459690faf42f9606817db7d705735c11b
Vladimir Admaev <vladimir.admaev@ylab.io>
2021-12-29 11:12:51 +0400

    IBT-553 Fix add discount without comment

--- app/Services/Discount/DiscountHelper.php
+++ app/Services/Discount/DiscountHelper.php
@@ -137,7 +137,7 @@ class DiscountHelper
         $discount->status = $data['status'];
         $discount->product_qty_limit = $data['product_qty_limit'] ?? null;
         $discount->promo_code_only = $data['promo_code_only'];
-        $discount->comment = $data['comment'];
+        $discount->comment = $data['comment'] ?? null;
 
         $ok = $discount->save();
         if (!$ok) {
