47f4187d2fbe0063728a67e4393b36eefe35afcf
Leonid Borisov <stiv902931@gmail.com>
2023-06-01 09:19:48 +0300

    15590: add updated_at field to pricesByOffers

--- app/Models/Price/Price.php
+++ app/Models/Price/Price.php
@@ -14,6 +14,7 @@ use Greensight\CommonMsa\Models\AbstractModel;
  * @property double $price - цена проф
  * @property double $price_base - цена базовая
  * @property double $price_retail - цена розничная
+ * @property string $updated_at - Дата и время последнего обновления
  */
 class Price extends AbstractModel
 {
