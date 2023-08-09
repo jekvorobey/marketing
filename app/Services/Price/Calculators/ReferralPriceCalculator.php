be29664ce114de107fe12a02a0c7af30e3bb74db
Vadim Matulis <pacovado@gmail.com>
2023-08-09 17:51:29 +0700

    18399: fix

--- app/Services/Price/Calculators/ReferralPriceCalculator.php
+++ app/Services/Price/Calculators/ReferralPriceCalculator.php
@@ -3,24 +3,11 @@
 namespace App\Services\Price\Calculators;
 
 use Greensight\CommonMsa\Dto\RoleDto;
-use MerchantManagement\Dto\MerchantPricesDto;
 
-class ReferralPriceCalculator extends AbstractPriceCalculator
+class ReferralPriceCalculator extends ProfPriceCalculator
 {
     public function getRole(): int
     {
         return RoleDto::ROLE_SHOWCASE_REFERRAL_PARTNER;
-    }
-
-    public function calculatePrice(float $basePrice): float
-    {
-        /** @var MerchantPricesDto $relevantMerchantPriceSettings */
-        $relevantMerchantPriceSettings = $this->getRelevantMerchantPriceSettings();
-
-        if ($relevantMerchantPriceSettings && $relevantMerchantPriceSettings->valueProf) {
-            $basePrice = ceil($basePrice + $basePrice * $relevantMerchantPriceSettings->valueProf / 100);
-        }
-
-        return $basePrice;
     }
 }
