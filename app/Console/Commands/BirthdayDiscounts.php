67dbf726ca5d18a5a772dacabf7df5b0c304597e
Vadim Matulis <pacovado@gmail.com>
2023-11-08 12:58:26 +0700

    19354: birthday discounts

--- app/Console/Commands/BirthdayDiscounts.php
+++ app/Console/Commands/BirthdayDiscounts.php
@@ -6,6 +6,7 @@ use App\Models\Discount\Discount;
 use App\Models\Discount\DiscountCondition;
 use App\Models\Discount\DiscountConditionGroup;
 use App\Models\Discount\LogicalOperator;
+use App\Models\Order\Order;
 use Carbon\Carbon;
 use Greensight\CommonMsa\Rest\RestQuery;
 use Greensight\Message\Services\ServiceNotificationService\ServiceNotificationService;
@@ -17,12 +18,15 @@ use App\Models\PromoCode\PromoCode;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
-class CreateBirthdayDiscounts extends Command
+class BirthdayDiscounts extends Command
 {
     protected const DAYS_BEFORE_BITHDAY = 7;
     protected const DAYS_AFTER_BITHDAY = 14;
     protected const CHECK_DISCOUNTS_LAST_DAYS_COUNT = 357;
     protected const CREATOR_USER_ID = 19435; //Талалов
+
+    protected const DISCOUNT_1_COPY_FROM_ID = 2229;
+    protected const DISCOUNT_2_COPY_FROM_ID = 2230;
 
     /**
      * The name and signature of the console command.
@@ -36,9 +40,9 @@ class CreateBirthdayDiscounts extends Command
      *
      * @var string
      */
-    protected $description = 'Создает скидки ко дню рождения';
+    protected $description = 'Создает скидки ко дню рождения и отправляет уведомления';
 
-    private PromoCode $birthdayPromocode;
+    private ?PromoCode $birthdayPromocode;
     private CustomerService $customerService;
     private ServiceNotificationService $notificationService;
     private OrderService $orderService;
@@ -85,6 +89,89 @@ class CreateBirthdayDiscounts extends Command
         return 0;
     }
 
+    protected function createBirthdayDiscounts(): void
+    {
+        $customers = $this->getCusotmers(static::DAYS_BEFORE_BITHDAY);
+
+        /** @var CustomerDto $customer */
+        foreach ($customers as $customer) {
+            $alreadyCreatedDiscounts = $this->getBirthdayDiscountsForLastDays($customer, static::CHECK_DISCOUNTS_LAST_DAYS_COUNT);
+            if ($alreadyCreatedDiscounts->isNotEmpty()) {
+                continue;
+            }
+
+            try {
+                $discount1 = $this->copyDiscountFrom(static::DISCOUNT_1_COPY_FROM_ID, $customer);
+                $discount2 = $this->copyDiscountFrom(static::DISCOUNT_2_COPY_FROM_ID, $customer);
+
+                $this->notificationService->send($customer->user_id, 'birthday_discount_created', [
+                    'EXPIRES_AT' => $discount1->end_date->format('d.m.Y')
+                ]);
+
+            } catch (\Throwable $exception) {
+                report($exception);
+            }
+        }
+    }
+
+    protected function copyDiscountFrom(int $discountId, CustomerDto $customer): Discount
+    {
+        $originalDiscount = Discount::whereId($discountId)->firstOrFail();
+
+        try {
+            DB::beginTransaction();
+
+            $discount = $originalDiscount->replicate();
+            $discount->name = $this->generateDiscountName($customer);
+            $discount->status = Discount::STATUS_ACTIVE;
+            $discount->start_date = now();
+            $discount->end_date = now()->addDays(static::DAYS_BEFORE_BITHDAY + static::DAYS_AFTER_BITHDAY);
+            $discount->promo_code_only = true;
+            $discount->summarizable_with_all = true;
+            $discount->push();
+
+            $this->replicateRelations($originalDiscount, $discount, 'brands');
+            $this->replicateRelations($originalDiscount, $discount, 'categories');
+
+            $discount->promoCodes()->save($this->birthdayPromocode);
+
+            $conditionGroup = new DiscountConditionGroup([
+                'discount_id' => $discount->id,
+                'logical_operator' => LogicalOperator::AND,
+            ]);
+            $conditionGroup->save();
+
+            $condition = new DiscountCondition([
+                'discount_id' => $discount->id,
+                'type' => DiscountCondition::CUSTOMER,
+                'condition' => [DiscountCondition::FIELD_CUSTOMER_IDS => [$customer->id]],
+                'discount_condition_group_id' => $conditionGroup->id,
+            ]);
+            $condition->save();
+
+            DB::commit();
+
+        } catch (\Throwable $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
+
+        return $discount;
+    }
+
+    protected function replicateRelations(Discount $originalDiscount, Discount $newDiscount, string $relationName): void
+    {
+        $originalDiscount->load($relationName);
+
+        if (!$originalDiscount->{$relationName}) {
+            return;
+        }
+
+        foreach ($originalDiscount->{$relationName} as $relationModel) {
+            $newDiscount->{$relationName}()->save($relationModel->replicate());
+        }
+    }
+
     protected function sendBithdayNotifications(): void
     {
         $customers = $this->getCusotmers();
@@ -120,75 +207,6 @@ class CreateBirthdayDiscounts extends Command
         );
     }
 
-    protected function createBirthdayDiscounts(): void
-    {
-        $customers = $this->getCusotmers(static::DAYS_BEFORE_BITHDAY);
-
-        /** @var CustomerDto $customer */
-        foreach ($customers as $customer) {
-            $alreadyCreatedDiscounts = $this->getBirthdayDiscountsForLastDays($customer, static::CHECK_DISCOUNTS_LAST_DAYS_COUNT);
-            if ($alreadyCreatedDiscounts->isNotEmpty()) {
-                continue;
-            }
-
-            try {
-                $discount = $this->createBithdayDiscount($customer);
-
-                $this->notificationService->send($customer->user_id, 'birthday_discount_created', [
-                    'EXPIRES_AT' => $discount->end_date->format('d.m.Y')
-                ]);
-
-            } catch (\Throwable $exception) {
-                report($exception);
-            }
-        }
-    }
-
-    protected function createBithdayDiscount(CustomerDto $customer): Discount
-    {
-        try {
-            DB::beginTransaction();
-
-            $discount = new Discount([
-                'user_id' => static::CREATOR_USER_ID,
-                'type' => Discount::DISCOUNT_TYPE_ANY_OFFER,
-                'name' => $this->generateDiscountName($customer),
-                'value_type' => Discount::DISCOUNT_VALUE_TYPE_PERCENT,
-                'value' => 10,
-                'status' => Discount::STATUS_ACTIVE,
-                'start_date' => now(),
-                'end_date' => now()->addDays(static::DAYS_BEFORE_BITHDAY + static::DAYS_AFTER_BITHDAY),
-                'promo_code_only' => true,
-                'summarizable_with_all' => true,
-            ]);
-            $discount->save();
-
-            $discount->promoCodes()->save($this->birthdayPromocode);
-
-            $conditionGroup = new DiscountConditionGroup([
-                'discount_id' => $discount->id,
-                'logical_operator' => LogicalOperator::AND,
-            ]);
-            $conditionGroup->save();
-
-            $condition = new DiscountCondition([
-                'discount_id' => $discount->id,
-                'type' => DiscountCondition::CUSTOMER,
-                'condition' => [DiscountCondition::FIELD_CUSTOMER_IDS => [$customer->id]],
-                'discount_condition_group_id' => $conditionGroup->id,
-            ]);
-            $condition->save();
-
-            DB::commit();
-
-        } catch (\Throwable $exception) {
-            DB::rollBack();
-            throw $exception;
-        }
-
-        return $discount;
-    }
-
     protected function generateDiscountName(CustomerDto $customer): string
     {
         return join('_', [PromoCode::HAPPY2U_PROMOCODE, $customer->id, now()->toDateString()]);
@@ -210,7 +228,9 @@ class CreateBirthdayDiscounts extends Command
     protected function getCusotmers(int $daysBeforeBirthday = 0): Collection
     {
         return $this->customerService->customers(
-            $this->customerService->newQuery()->setFilter('birthday', now()->addDays($daysBeforeBirthday)->toDateString())
+            $this->customerService->newQuery()
+                ->setFilter('birthday', now()->addDays($daysBeforeBirthday)->toDateString())
+                ->setFilter('status', CustomerDto::STATUS_ACTIVE)
         );
     }
 }
